1fd09184b9cc24a37277ba2083c69bc5a53093f9
최문길 <cmk0905@naver.com>
2023-11-07 02:05:08 +0900

    삭제 할거 함

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react';
 import './App.css';
 import LayOut from './components/LayOut/LayOut.jsx'
 import Form from './components/formControl/Form.jsx'
-import Todo from './components/todoList/Todo.jsx'
+
+
 function App() {
 
-
   return (
-    <div className="App">
      <LayOut>
       <Form/>
      </LayOut>
-    </div>
   );
 }
 
